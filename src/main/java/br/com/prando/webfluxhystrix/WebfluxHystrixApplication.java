6b92831e640f3ebc28f3b1a962d0646ae62305c5
José Prando <jose.prando@rapiddo.com.br>
2018-06-29 15:43:15 -0300

    initial commit

---
+++ src/main/java/br/com/prando/webfluxhystrix/WebfluxHystrixApplication.java
@@ -0,0 +1,12 @@
+package br.com.prando.webfluxhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxHystrixApplication.class, args);
+    }
+}
